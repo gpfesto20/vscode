454880c305ead4fcc1532e00d9c43a57f32504d7
gpfesto20 <gpnfesto2022@gmail.com>
2024-11-12 07:39:32 +0530

    Add files via upload

---
+++ launch10/KMeansClustering.java
@@ -0,0 +1,14 @@
+ partition-based clustering in unsupervised learning, focusing on K-Means clustering. This approach divides data into distinct clusters by minimizing the distance between data points and their cluster centers (in K-Means) or representative points (in K-Medoids).
+
+K-Means Clustering Summary:
+K-Means is an iterative algorithm that groups data into k clusters by assigning each data point to the nearest centroid.
+Steps include:
+Select a value for k.
+Choose k initial points as centroids.
+Compute distances and assign data points to the nearest centroid.
+Recalculate centroids based on current assignments.
+Repeat until convergence (no further changes in cluster assignments)
+
+
+colfilter-kmeans
+clusrering
